d9b3c973ba63266d28eb6c60fac393c4e5878547
soikotdeb <sdsoikot424@gmail.com>
2023-06-05 13:34:33 +0600

    paymant system and store paymant data

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ async function run() {
     // create payment intent
     app.post("/create-payment-intent", verifyJwt, async (req, res) => {
       const { price } = req.body;
-      const amount = price * 100;
+      const amount = parseInt(price * 100);
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
         currency: "usd",
@@ -161,6 +161,71 @@ async function run() {
       const deleteResult = await cartCollection.deleteMany(query);
       res.send({ insertResult, deleteResult });
     });
+
+
+
+    app.get('/admin-stats', verifyJwt, verifyAdmin, async(req,res)=>{
+      const users =  await usersCollection.estimatedDocumentCount();
+      const products = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+
+      // best way to sum of the price field is to use group and sum operator
+      const payments = await paymentCollection.find().toArray();
+      const revenue = payments.reduce(( sum, payment)=>sum + payment.price,0)
+
+
+
+      res.send({
+        users,
+       products,
+       orders,
+       revenue
+
+      })
+    })
+
+    // admin order chart get data api--- second best solution
+
+    app.get('/order-stats', verifyJwt, verifyAdmin, async(req, res) =>{
+      const pipeline = [
+        {
+          $lookup: {
+            from: 'menu',
+            localField: 'menuItems',
+            foreignField: '_id',
+            as: 'menuItemsData'
+          }
+        },
+        {
+          $unwind: '$menuItemsData'
+        },
+        {
+          $group: {
+            _id: '$menuItemsData.category',
+            count: { $sum: 1 },
+            total: { $sum: '$menuItemsData.price' }
+          }
+        },
+        {
+          $project: {
+            category: '$_id',
+            count: 1,
+            total: { $round: ['$total', 2] },
+            _id: 0
+          }
+        }
+      ];
+
+      const result = await paymentCollection.aggregate(pipeline).toArray()
+      res.send(result)
+
+    })
+
+
+
+
+
+
 
     /**
      * do not show secure links to those  who should not see the links
