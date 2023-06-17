a13496d6c94e6085600b86df63d88b224bc9f3f3
soikotdeb <sdsoikot424@gmail.com>
2023-06-17 19:43:13 +0600

    server deploy

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // collection
     const menuCollection = client.db("bistroDB").collection("menu");
@@ -278,7 +278,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
